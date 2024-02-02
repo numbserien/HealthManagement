33be0b1759bc20df198b55b9c3a441ff6ba1f175
numb_serien <411932187@qq.com>
2024-02-02 12:59:35 +0800

    注解的解释

--- src/main/java/com/zq/demo/annotation/NotResultWrap.java
+++ src/main/java/com/zq/demo/annotation/NotResultWrap.java
@@ -6,6 +6,10 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+
+/**
+ * 不需要走返回数据统一包装的标签
+ */
 @Target({METHOD})
 @Retention(RUNTIME)
 public @interface NotResultWrap {
