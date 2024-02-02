1b330f5b887abb4631ba6d1c9c3e9604cc20a068
numb_serien <411932187@qq.com>
2024-02-03 01:13:18 +0800

    fix(todo): 解决 authenticationManager类 没有bean工厂的问题

--- src/main/java/com/zq/demo/config/KaptchaConfig.java
+++ src/main/java/com/zq/demo/config/KaptchaConfig.java
@@ -4,6 +4,8 @@ import com.google.code.kaptcha.impl.DefaultKaptcha;
 import com.google.code.kaptcha.util.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 import java.util.Properties;
 
@@ -26,6 +28,12 @@ public class KaptchaConfig {
         DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
         defaultKaptcha.setConfig(config);
         return defaultKaptcha;
+    }
+
+    //    TODO 对于AuthenticationManager无法获取的特殊处理方式
+    @Bean
+    AuthenticationManager manager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
