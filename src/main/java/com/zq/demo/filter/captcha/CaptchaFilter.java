b60ee43a8bfe7fb158cfb587df3bed8cffa5e615
numb_serien <411932187@qq.com>
2024-02-03 01:03:19 +0800

    feat(filter): jwt 验证器
    验证token是否过期或token不正确

---
+++ src/main/java/com/zq/demo/filter/captcha/CaptchaFilter.java
@@ -0,0 +1,56 @@
+package com.zq.demo.filter.captcha;
+
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.zq.demo.exception.CaptchaException;
+import com.zq.demo.filter.jwt.LoginFailureHandler;
+import com.zq.demo.util.Const;
+import com.zq.demo.util.RedisUtils02;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class CaptchaFilter extends OncePerRequestFilter {
+
+    @Autowired
+    LoginFailureHandler loginFailureHandler;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException, ServletException, IOException {
+
+        String url = httpServletRequest.getRequestURI();
+        if ("/login".equals(url) && httpServletRequest.getMethod().equals("POST")) {
+            // 校验验证码
+            try {
+                validate(httpServletRequest);
+            } catch (CaptchaException e) {
+                // 交给认证失败处理器
+                loginFailureHandler.onAuthenticationFailure(httpServletRequest, httpServletResponse, e);
+            }
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    // 校验验证码逻辑
+    private void validate(HttpServletRequest httpServletRequest) {
+        String code = httpServletRequest.getParameter("code");
+        String key = httpServletRequest.getParameter("userKey");
+
+        if (StringUtils.isBlank(code) || StringUtils.isBlank(key)) {
+            throw new CaptchaException("验证码错误");
+        }
+
+        if (!code.equals(RedisUtils02.hget(Const.CAPTCHA_KEY, key))) {
+            throw new CaptchaException("验证码错误");
+        }
+
+        // 若验证码正确, 删除验证码
+        RedisUtils02.hdel(Const.CAPTCHA_KEY, key);
+    }
+}
---
+++ src/main/java/com/zq/demo/filter/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,73 @@
+package com.zq.demo.filter.jwt;
+
+import cn.hutool.core.util.StrUtil;
+import com.zq.demo.pojo.User;
+import com.zq.demo.service.impl.UserServiceImpl;
+import com.zq.demo.util.JwtUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * 验证是否正确或过期
+ */
+@Component
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    UserDetailServiceImpl userDetailService;
+
+    @Autowired
+    UserServiceImpl userService;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String jwt = request.getHeader(jwtUtils.getHeader());
+        // 这里如果没有jwt，继续往后走，因为后面还有鉴权管理器等去判断是否拥有身份凭证，所以是可以放行的
+        // 没有jwt相当于匿名访问，若有一些接口是需要权限的，则不能访问这些接口
+        if (StrUtil.isBlankOrUndefined(jwt)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        Claims claim = jwtUtils.getClaimsByToken(jwt);
+        if (claim == null) {
+            throw new JwtException("token 异常");
+        }
+        if (jwtUtils.isTokenExpired(claim)) {
+            throw new JwtException("token 已过期");
+        }
+
+        String username = claim.getSubject();
+        // 获取用户的权限等信息
+
+        User sysUser = userService.getByUsername(username);
+
+
+        // 构建UsernamePasswordAuthenticationToken,这里密码为null，是因为提供了正确的JWT,实现自动登录
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, null, userDetailService.getUserAuthority(sysUser.getU_id()));
+        SecurityContextHolder.getContext().setAuthentication(token);
+
+        chain.doFilter(request, response);
+
+    }
+}
