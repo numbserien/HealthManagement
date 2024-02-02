e8efd242b5ad672c9b9299a77669506d7c7d75a1
numb_serien <411932187@qq.com>
2024-02-02 16:13:57 +0800

    feat(filter): jwt 登录成功过滤器

---
+++ src/main/java/com/zq/demo/filter/jwt/LoginSuccessHandler.java
@@ -0,0 +1,39 @@
+package com.zq.demo.filter.jwt;
+
+import cn.hutool.json.JSONUtil;
+import com.zq.demo.pojo.sys.Result;
+import com.zq.demo.util.JwtUtils;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        httpServletResponse.setContentType("application/json;charset=UTF-8");
+        ServletOutputStream outputStream = httpServletResponse.getOutputStream();
+
+        // 生成JWT，并放置到请求头中
+        String jwt = jwtUtils.generateToken(authentication.getName());
+        httpServletResponse.setHeader(jwtUtils.getHeader(), jwt);
+
+        Result result = Result.success("SuccessLogin");
+
+        outputStream.write(JSONUtil.toJsonStr(result).getBytes(StandardCharsets.UTF_8));
+        outputStream.flush();
+        outputStream.close();
+    }
+}
