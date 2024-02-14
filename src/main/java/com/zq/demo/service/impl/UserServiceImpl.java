4fef5c15fccba81f01b01690c56d00641df413a3
numb_serien <411932187@qq.com>
2024-02-14 17:10:25 +0800

    feat(userClass): 查询语句: 根据用户名查询用户

--- src/main/java/com/zq/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/zq/demo/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.zq.demo.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zq.demo.dao.UserDao;
 import com.zq.demo.pojo.User;
 import com.zq.demo.service.IUserService;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,9 +20,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserDao, User> implements IUserService {
 
+    @Resource
+    private UserDao userDao;
+
     //    TODO 根据用户名返回用户信息(id,username,password)
     public User getByUsername(String username) {
-        return null;
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(username != null, User::getUsername, username)
+                .select(User::getId, User::getUsername, User::getPassword, User::getStatus);
+        return userDao.selectOne(queryWrapper);
     }
 
 }
