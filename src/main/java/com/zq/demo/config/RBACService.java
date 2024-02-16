36a041890bee74b55f5cb5d04699d3afdbbb26ae
numb_serien <411932187@qq.com>
2024-02-16 13:31:11 +0800

    fix(rbac): 修正访问权限判断方法

--- src/main/java/com/zq/demo/config/RBACService.java
+++ src/main/java/com/zq/demo/config/RBACService.java
@@ -1,21 +1,27 @@
 package com.zq.demo.config;
 
 
+import com.zq.demo.filter.UsernamePasswordAuthentication.UserDetailServiceImpl;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 @Component("rbacService")
 public class RBACService {
+    @Resource
+    private UserDetailServiceImpl userDetailService;
 
     public boolean hasPermission(HttpServletRequest request, Authentication authentication) {
         Object principal = authentication.getPrincipal();
-        if (principal instanceof UserDetails) {
-            UserDetails userDetails = (UserDetails) principal;
-
+        if (principal instanceof String) {
+            UserDetails userDetails = userDetailService.loadUserByUsername((String) principal);
+            if (userDetails == null) {
+                return false;
+            }
             // 本次要访问的资源
             SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(request.getRequestURI());
             return userDetails.getAuthorities().contains(simpleGrantedAuthority);
