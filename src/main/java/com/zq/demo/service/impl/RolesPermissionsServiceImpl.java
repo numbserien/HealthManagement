ceb6fa891f1b6c7ed41a96303bd520ffa39ad0e3
numb_serien <411932187@qq.com>
2024-02-16 11:34:23 +0800

    feat: 完成TODO，根据角色Id列表返回权限路径

--- src/main/java/com/zq/demo/service/impl/RolesPermissionsServiceImpl.java
+++ src/main/java/com/zq/demo/service/impl/RolesPermissionsServiceImpl.java
@@ -1,12 +1,15 @@
 package com.zq.demo.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zq.demo.dao.PermissionsDao;
 import com.zq.demo.dao.RolesPermissionsDao;
+import com.zq.demo.pojo.Permissions;
 import com.zq.demo.pojo.RolesPermissions;
 import com.zq.demo.service.IRolesPermissionsService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -20,8 +23,21 @@ import java.util.List;
 @Service
 public class RolesPermissionsServiceImpl extends ServiceImpl<RolesPermissionsDao, RolesPermissions> implements IRolesPermissionsService {
 
-    //    TODO 根据角色名列表返回权限路径
-    public List<String> getPermissionListByRoleList(List<String> roles) {
-        return new ArrayList<>();
+    @Resource
+    private RolesPermissionsDao rolesPermissionsDao;
+    @Resource
+    private PermissionsDao permissionsDao;
+
+    //    根据角色Id列表返回权限路径
+    public List<String> getPermissionListByRoleList(List<Long> rolesId) {
+        LambdaQueryWrapper<RolesPermissions> queryWrapperRP = new LambdaQueryWrapper<>();
+        queryWrapperRP.in(!rolesId.isEmpty(), RolesPermissions::getId, rolesId)
+                .select(RolesPermissions::getPermissionId);
+        List<Long> permissionIds = rolesPermissionsDao.selectObjs(queryWrapperRP);
+        LambdaQueryWrapper<Permissions> queryWrapperR = new LambdaQueryWrapper<>();
+        queryWrapperR.in(Permissions::getId, permissionIds)
+                .eq(Permissions::getStatus, 1)
+                .select(Permissions::getUrl);
+        return permissionsDao.selectObjs(queryWrapperR);
     }
 }
