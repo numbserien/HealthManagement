fbd54e78e19bbcf88ffaa4d9cda5ec5b013dba47
numbserien <411932187@qq.com>
2024-03-18 17:35:05 +0800

    feat(RBAC验证): 增加网页对 * 的解析

--- src/main/java/com/zq/demo/config/RBACService.java
+++ src/main/java/com/zq/demo/config/RBACService.java
@@ -3,12 +3,16 @@ package com.zq.demo.config;
 
 import com.zq.demo.filter.UsernamePasswordAuthentication.UserDetailServiceImpl;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
 
 @Component("rbacService")
 public class RBACService {
@@ -24,6 +28,14 @@ public class RBACService {
             }
             // 本次要访问的资源
             SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(request.getRequestURI());
+            // 处理 * 的访问情况
+            for (GrantedAuthority item:userDetails.getAuthorities()){
+                String url = item.getAuthority();
+                if (url.endsWith("*")){
+                    String realUrl = url.substring(0, url.length() - 2);
+                    return simpleGrantedAuthority.getAuthority().startsWith(realUrl);
+                }
+            }
             return userDetails.getAuthorities().contains(simpleGrantedAuthority);
         }
         return false;
