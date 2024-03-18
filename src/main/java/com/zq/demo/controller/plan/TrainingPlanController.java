33415c2897c83cc756f68df08f01dd199a1b7d12
numbserien <411932187@qq.com>
2024-03-19 01:01:27 +0800

    feat: 增加了简单的基础接口，还未完善

--- src/main/java/com/zq/demo/controller/plan/TrainingPlanController.java
+++ src/main/java/com/zq/demo/controller/plan/TrainingPlanController.java
@@ -3,16 +3,13 @@ package com.zq.demo.controller.plan;
 
 import com.zq.demo.pojo.plan.TrainingPlan;
 import com.zq.demo.pojo.sys.Result;
+import com.zq.demo.pojo.sys.ResultCode;
 import com.zq.demo.service.impl.plan.TrainingPlanServiceImpl;
 import com.zq.demo.util.JwtUtils;
-import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.websocket.server.PathParam;
+import java.util.Objects;
 
 /**
  * <p>
@@ -29,17 +26,62 @@ public class TrainingPlanController {
     private TrainingPlanServiceImpl service;
     @Autowired
     private JwtUtils jwtUtils;
+//    初始化新计划书
     @PostMapping("/")
     public Result makePlan(HttpServletRequest request, @RequestParam String name){
-        String jwt = String.valueOf(request.getHeader("Authorization"));
-        System.out.println(jwt);
-        Claims claimsByToken = jwtUtils.getClaimsByToken(jwt);
-        System.out.println(claimsByToken.getSubject());
-        TrainingPlan trainingPlan = service.init(name,new Integer(claimsByToken.getSubject()));
+        Integer creatorId = jwtUtils.getAuthId(request);
+//        应该不会存在无法解析jwt的情况
+//        if (creatorId==-1){
+//            return Result.failure("认证错误，请重新登录！");
+//        }
+        TrainingPlan trainingPlan = service.init(name,creatorId);
         if (trainingPlan!=null){
             return Result.success(trainingPlan);
         }else
             return Result.failure("新建失败，请重试！");
+    }
+//    更新计划  ps:只允许本人操作
+//    TODO 按道理只允许修改计划名、执行天数、类型，并且判断计划项目是否为未上线状态，上线期间也不能更新
+    @PostMapping("/update")
+    public Result updatePlan(@RequestBody TrainingPlan trainingPlan,HttpServletRequest request){
+        Integer authId = jwtUtils.getAuthId(request);
+        if (!Objects.equals(authId, trainingPlan.getTp_u_id())){
+            return Result.failure(ResultCode.AUTHERROR);
+        }
+        service.updateById(trainingPlan);
+        return Result.success(trainingPlan);
+    }
+//    更改计划的状态
+    @PostMapping("/{tp_id}/{status}")
+    public Result changeStatus(@PathVariable("tp_id") Integer tp_id, @PathVariable("status") Integer status, HttpServletRequest request){
+//        动手者id
+        Integer authId = jwtUtils.getAuthId(request);
+//      TODO 这里根据角色来做判断是否能操作改变状态
+
+        Integer isSuccess = service.setStatus(tp_id, status);
+        if (isSuccess!=0){
+            return Result.success("修改成功");
+        }else {
+            return Result.failure("操作失败");
+        }
+    }
+//    通过id寻找整个计划，包含内容信息 ps:非公开计划只能自己查看
+//    TODO 返回整个计划信息
+    @GetMapping("/{tp_id}")
+    public Result getPlanById(@PathVariable("tp_id") Integer tp_id, HttpServletRequest request){
+        TrainingPlan byId = service.getById(tp_id);
+        if (byId==null){
+            return Result.failure(ResultCode.NOTFOUND);
+        }
+        if (byId.getTp_type()==0){
+            // 私有项目
+            Integer authId = jwtUtils.getAuthId(request);
+            if (Objects.equals(byId.getTp_u_id(), authId)){
+                return Result.success(byId);
+            }
+            return Result.failure("该训练计划未公开");
+        }
+        return Result.success(byId);
     }
 }
 
--- src/main/java/com/zq/demo/service/impl/plan/TrainingPlanServiceImpl.java
+++ src/main/java/com/zq/demo/service/impl/plan/TrainingPlanServiceImpl.java
@@ -1,6 +1,7 @@
 package com.zq.demo.service.impl.plan;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.zq.demo.pojo.plan.TrainingPlan;
 import com.zq.demo.dao.plan.TrainingPlanDao;
 import com.zq.demo.service.interfaces.plan.ITrainingPlanService;
@@ -8,6 +9,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 /**
  * <p>
@@ -21,12 +23,20 @@ import javax.annotation.Resource;
 public class TrainingPlanServiceImpl extends ServiceImpl<TrainingPlanDao, TrainingPlan> implements ITrainingPlanService {
     @Resource
     private TrainingPlanDao planDao;
+//    创建初始化计划
     public TrainingPlan init(String name,Integer createId){
-        LambdaQueryWrapper<TrainingPlan> wrapper = new LambdaQueryWrapper<>();
         TrainingPlan trainingPlan = new TrainingPlan(name);
         trainingPlan.setTp_u_id(createId);
         this.planDao.insert(trainingPlan);
         System.out.println(trainingPlan);
         return trainingPlan;
+    }
+//    修改状态
+    public Integer setStatus(Integer id,Integer status){
+        LambdaUpdateWrapper<TrainingPlan> wrapper = new LambdaUpdateWrapper<>();
+        wrapper.eq(TrainingPlan::getTp_id,id);
+        wrapper.set(TrainingPlan::getTp_status,status).set(TrainingPlan::getTp_update_time, new Date());
+        int update = planDao.update(wrapper);
+        return update;
     }
 }
