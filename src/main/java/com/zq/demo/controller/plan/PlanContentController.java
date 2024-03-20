c79246fa6ce4f854063e55a201087b549c661493
numbserien <411932187@qq.com>
2024-03-20 21:03:12 +0800

    feat: Plan Content基础接口

--- src/main/java/com/zq/demo/controller/plan/PlanContentController.java
+++ src/main/java/com/zq/demo/controller/plan/PlanContentController.java
@@ -1,9 +1,18 @@
 package com.zq.demo.controller.plan;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.zq.demo.pojo.plan.PlanContent;
+import com.zq.demo.pojo.sys.Result;
+import com.zq.demo.pojo.sys.ResultCode;
+import com.zq.demo.service.impl.plan.PlanContentServiceImpl;
+import com.zq.demo.util.JwtUtils;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Objects;
 
 /**
  * <p>
@@ -14,8 +23,85 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-02-09
  */
 @RestController
-@RequestMapping("/planContent")
+@RequestMapping("/user/planContents")
 public class PlanContentController {
+    @Resource
+    private JwtUtils jwtUtils;
+    @Resource
+    private PlanContentServiceImpl service;
+    @Resource
+    private HttpServletRequest request;
+//    创建内容
+    @PostMapping("/")
+    public Result init(@RequestBody PlanContent planContent){
+        Long authId = jwtUtils.getAuthId(request);
+        if (service.findByUIdAndPlanName(authId,planContent.getPc_name())!=null){
+            return Result.failure("个人计划内容名重用，请更改后重试");
+        }
+        planContent.setPc_u_id(authId);
+        service.init(planContent);
+        return Result.success(planContent);
+    }
+//    修改内容
+    @PutMapping("/{pc_id}")
+    public Result updatePC(@PathVariable("pc_id") Long pc_id,@RequestBody PlanContent planContent){
+        Long authId = jwtUtils.getAuthId(request);
+//        判断是否为本人修改
+        if (service.findByUIdAndPlanId(authId,pc_id)==null){
+            return Result.failure("权限错误，不能修改他人创作");
+        }
+//      判断修改的计划名不能重复
+        PlanContent checkObj = service.findByUIdAndPlanName(authId,planContent.getPc_name());
+        if (checkObj!=null){
+            if (!Objects.equals(checkObj.getPc_id(), pc_id)){
+                return Result.failure("个人计划内容名重用，请更改后重试");
+            }
+        }
+//      确保修改的对应id
+        planContent.setPc_id(pc_id);
+//      确保创建时间和制作人不修改
+        planContent.setPc_u_id(null);
+        planContent.setPc_create_time(null);
 
+        planContent.setPc_update_time(new Date());
+        if (service.updateById(planContent)){
+            return Result.success();
+        }else {
+            return Result.failure("操作失败,请重试");
+        }
+    }
+//    更改 公开权限
+    @PatchMapping("/{pc_id}")
+    public Result changeOpen(@PathVariable("pc_id") Long pc_id,@RequestParam("is_open") Integer is_open){
+        Long authId = jwtUtils.getAuthId(request);
+        Integer updateCount = service.changeOpen(authId, pc_id, is_open);
+        if (updateCount!=0){
+          return   Result.success("修改成功，"+ updateCount+"条数据更改");
+        }
+        return Result.failure(ResultCode.AUTHERROR);
+    }
+//    条件查找
+    @GetMapping("/")
+    public Result getPlanContents(@RequestParam(value = "authorIds",required = false) Long[] authorIds,
+                                  @RequestParam(value = "search",required = false) String search,
+                                  @RequestParam(value = "page",required = false,defaultValue = "1") Integer page,
+                                  @RequestParam(value = "count",required = false,defaultValue = "10") Integer count,
+                                  @RequestParam(value = "orderItem",required = false) String orderItem,
+                                  @RequestParam(value = "orderType",required = false) String orderType
+    ){
+        Long authId = jwtUtils.getAuthId(request);
+        IPage<PlanContent> IPage = service.search(page, count, search, orderItem, orderType, authId,authorIds);
+        return Result.success(IPage);
+    }
+//    根据pc_id查找
+    @GetMapping("/{pc_id}")
+    public Result getPlanContentById(@PathVariable(value = "pc_id") Long pc_id){
+        Long authId = jwtUtils.getAuthId(request);
+        PlanContent planContent = service.searchById(pc_id, authId);
+        if (planContent!=null){
+            return  Result.success(planContent);
+        }
+        return Result.failure(ResultCode.NOTFOUND);
+    }
 }
 
--- src/main/java/com/zq/demo/service/impl/plan/PlanContentServiceImpl.java
+++ src/main/java/com/zq/demo/service/impl/plan/PlanContentServiceImpl.java
@@ -1,10 +1,19 @@
 package com.zq.demo.service.impl.plan;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zq.demo.pojo.plan.PlanContent;
 import com.zq.demo.dao.plan.PlanContentDao;
 import com.zq.demo.service.interfaces.plan.IPlanContentService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.Date;
 
 /**
  * <p>
@@ -17,4 +26,78 @@ import org.springframework.stereotype.Service;
 @Service
 public class PlanContentServiceImpl extends ServiceImpl<PlanContentDao, PlanContent> implements IPlanContentService {
 
+    @Resource
+    private PlanContentDao pcDao;
+
+    public void init(PlanContent planContent) {
+        Date createTime = new Date();
+        planContent.setPc_create_time(createTime);
+        planContent.setPc_update_time(createTime);
+        pcDao.insert(planContent);
+    }
+//    根据用户id和计划名查找
+    public PlanContent findByUIdAndPlanName(Long u_id,String planName){
+        LambdaQueryWrapper<PlanContent> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(PlanContent:: getPc_u_id,u_id)
+                .eq(PlanContent::getPc_name,planName);
+        return pcDao.selectOne(wrapper);
+    }
+//    根据用户id和计划id查找
+    public PlanContent findByUIdAndPlanId(Long u_id,Long pc_id){
+        LambdaQueryWrapper<PlanContent> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(PlanContent:: getPc_u_id,u_id)
+                .eq(PlanContent::getPc_id,pc_id);
+        return pcDao.selectOne(wrapper);
+    }
+//    更改是否公开权限
+    public Integer changeOpen(Long u_id,Long pc_id,Integer type){
+        LambdaUpdateWrapper<PlanContent> wrapper = new LambdaUpdateWrapper<>();
+        wrapper.eq(PlanContent::getPc_id,pc_id)
+                .eq(PlanContent::getPc_u_id,u_id);
+        wrapper.set(PlanContent::getPc_type,type);
+        return pcDao.update(null, wrapper);
+    }
+    //    分页查询模块
+    private <T> IPage<PlanContent> pageControl(int page, int count, Wrapper<T> wrapper){
+        IPage<PlanContent> pageC = new Page<>(page,count);
+        if (wrapper instanceof QueryWrapper) {
+            return pcDao.selectPage(pageC, (QueryWrapper<PlanContent>) wrapper);
+        } else if (wrapper instanceof LambdaQueryWrapper) {
+            return pcDao.selectPage(pageC, (LambdaQueryWrapper<PlanContent>) wrapper);
+        }else
+            return null;
+    }
+//    条件搜索 TODO 合并通过u_id查找和条件查询
+    public IPage<PlanContent> search(Integer page,
+                                     Integer count,
+                                     String search,
+                                     String orderItem,
+                                     String orderType,
+                                     Long authId,
+                                     Long[] authorIds){
+        // 可选排序条件
+        String[] condition = {"pc_create_time","pc_update_time","pc_site","pc_type","pc_name","pc_u_id"};
+        if (!Arrays.asList(condition).contains(orderItem)){
+            orderItem = null;
+        }
+        QueryWrapper<PlanContent> queryWrapper = new QueryWrapper<>();
+        queryWrapper
+                .and(wrapper->wrapper.like(search!=null,"pc_name",search).in(authorIds!=null && authorIds.length!=0,"pc_u_id",authorIds))
+                .and(wrapper->wrapper.eq(authorIds!=null && (authorIds.length==0 || Arrays.asList(authorIds).contains(authId)),"pc_u_id",authId)
+                        .or()
+                        // 公开
+                        .eq("pc_open",1))
+                .orderBy(orderItem!=null,!"desc".equals(orderType),orderItem);
+//        TODO 有时间设置返回值
+        return pageControl(page, count, queryWrapper);
+    }
+//    根据内容id查看详情
+    public PlanContent searchById(Long pc_id,Long authorId){
+        LambdaQueryWrapper<PlanContent> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(PlanContent::getPc_id,pc_id)
+                .and(wrapper1->wrapper1.eq(PlanContent::getPc_type,1)
+                        .or()
+                        .eq(PlanContent::getPc_u_id,authorId));
+        return pcDao.selectOne(wrapper);
+    }
 }
