aa2b2931ec98f86b27113e154ce6ad9ffa7729dc
numbserien <411932187@qq.com>
2024-03-20 20:20:28 +0800

    perf: 优化了分页模块，并合根据用户id查找的搜索功能

--- src/main/java/com/zq/demo/service/impl/plan/TrainingPlanServiceImpl.java
+++ src/main/java/com/zq/demo/service/impl/plan/TrainingPlanServiceImpl.java
@@ -1,5 +1,6 @@
 package com.zq.demo.service.impl.plan;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
@@ -12,7 +13,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 /**
@@ -28,20 +29,18 @@ public class TrainingPlanServiceImpl extends ServiceImpl<TrainingPlanDao, Traini
     @Resource
     private TrainingPlanDao planDao;
 //    创建初始化计划
-    public TrainingPlan init(String name,Integer createId){
+    public TrainingPlan init(String name,Long createId){
         TrainingPlan trainingPlan = new TrainingPlan(name);
         trainingPlan.setTp_u_id(createId);
         this.planDao.insert(trainingPlan);
-        System.out.println(trainingPlan);
         return trainingPlan;
     }
 //    修改状态
-    public Integer setStatus(Integer id,Integer status){
+    public Integer setStatus(Long id,Integer status){
         LambdaUpdateWrapper<TrainingPlan> wrapper = new LambdaUpdateWrapper<>();
         wrapper.eq(TrainingPlan::getTp_id,id);
         wrapper.set(TrainingPlan::getTp_status,status).set(TrainingPlan::getTp_update_time, new Date());
-        int update = planDao.update(wrapper);
-        return update;
+        return planDao.update(null,wrapper);
     }
 //    条件搜索
     public IPage<TrainingPlan> search(int page,
@@ -49,11 +48,18 @@ public class TrainingPlanServiceImpl extends ServiceImpl<TrainingPlanDao, Traini
                                       String search,
                                       String orderItem,
                                       String orderType,
-                                      Integer selfId){
+                                      Long selfId,
+                                      Long[] authorIds){
+        // 可选排序条件
+        String[] condition = {"tp_create_time","tp_update_time","tp_score","tp_status","tp_u_id","tp_days","tp_name"};
+        if (!Arrays.asList(condition).contains(orderItem)){
+            orderItem = null;
+        }
         QueryWrapper<TrainingPlan> queryWrapper = new QueryWrapper<>();
         queryWrapper
-                .and(search!=null,wrapper->wrapper.like("tp_name",search))
-                .and(wrapper->wrapper.eq("tp_u_id",selfId)
+                .and(wrapper->wrapper.like(search!=null,"tp_name",search)
+                        .in(authorIds!=null,"tp_u_id",authorIds))
+                .and(wrapper->wrapper.eq(authorIds==null || Arrays.asList(authorIds).contains(selfId),"tp_u_id",selfId)
                         .or()
                         // 公开
                         .eq("tp_type",1))
@@ -61,17 +67,21 @@ public class TrainingPlanServiceImpl extends ServiceImpl<TrainingPlanDao, Traini
         return pageControl(page, count, queryWrapper);
     }
 //    分页查询模块
-    private IPage<TrainingPlan> pageControl(int page,int count,QueryWrapper<TrainingPlan> queryWrapper){
+    private <T> IPage<TrainingPlan> pageControl(int page, int count, Wrapper<T> wrapper){
         IPage<TrainingPlan> pageC = new Page<>(page,count);
-        planDao.selectPage(pageC,queryWrapper);
-        return pageC;
+        if (wrapper instanceof QueryWrapper) {
+           return planDao.selectPage(pageC, (QueryWrapper<TrainingPlan>) wrapper);
+        } else if (wrapper instanceof LambdaQueryWrapper) {
+           return planDao.selectPage(pageC, (LambdaQueryWrapper<TrainingPlan>) wrapper);
+        }else
+            return null;
     }
 //    用户个人修改训练计划
-    public Integer updateByUser(TrainingPlan trainingPlan,Integer u_id){
+    public Integer updateByUser(TrainingPlan trainingPlan,Long u_id){
         LambdaUpdateWrapper<TrainingPlan> wrapper = new LambdaUpdateWrapper<>();
         wrapper.eq(TrainingPlan::getTp_u_id,u_id) // 本人更改
                 .eq(TrainingPlan::getTp_id,trainingPlan.getTp_id())
-                .in(TrainingPlan::getTp_status,0,1); // 非上线修改
+                .in(TrainingPlan::getTp_status,0,1); // 确保非上线
         wrapper.set(trainingPlan.getTp_name()!=null,TrainingPlan::getTp_name,trainingPlan.getTp_name())
                 .set(trainingPlan.getTp_type()!=null,TrainingPlan::getTp_type,trainingPlan.getTp_type())
                 .set(TrainingPlan::getTp_update_time,new Date());
