3693ffcca0c5bb768970b5b5dbfc35e3a8d944d1
numbserien <411932187@qq.com>
2024-03-22 10:56:29 +0800

    feat: planItem板块的基本数据库操作

--- src/main/java/com/zq/demo/controller/plan/PlanItemController.java
+++ src/main/java/com/zq/demo/controller/plan/PlanItemController.java
@@ -1,9 +1,19 @@
 package com.zq.demo.controller.plan;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.zq.demo.pojo.plan.PlanItem;
+import com.zq.demo.pojo.plan.TrainingPlan;
+import com.zq.demo.pojo.sys.Result;
+import com.zq.demo.pojo.sys.ResultCode;
+import com.zq.demo.service.impl.plan.PlanItemServiceImpl;
+import com.zq.demo.service.impl.plan.TrainingPlanServiceImpl;
+import com.zq.demo.util.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -14,8 +24,67 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-02-09
  */
 @RestController
-@RequestMapping("/planItem")
+@RequestMapping("/user/planItems")
 public class PlanItemController {
-
+    @Autowired
+    private PlanItemServiceImpl service;
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private TrainingPlanServiceImpl trainingPlanService;
+//    创建计划项
+    @PostMapping("/")
+    public Result init(@RequestBody PlanItem planItem){
+        Long authId = jwtUtils.getAuthId(request);
+        if (trainingPlanService.selectByUIdAndTPId(authId,planItem.getPi_tp_id())!=null){
+            // 确认有权限
+            service.init(planItem);
+            return Result.success(planItem);
+        }else {
+            return Result.failure(ResultCode.AUTHERROR);
+        }
+    }
+//   支持批量和单独更新计划项(调整顺序)
+    @PutMapping("/")
+    public Result updateToOne(@RequestBody ArrayList<PlanItem> planItems){
+        if (planItems==null || planItems.size()==0){
+            return Result.failure("参数传入错误，请重试");
+        }
+//        确保是同一个计划中的计划项
+        Long[] longs = planItems.stream().map(PlanItem::getPi_id).toArray(Long[]::new);
+        boolean allTpIdSame = service.isSameTp(longs);
+        if(allTpIdSame){
+            boolean b = service.updateBatchById(planItems);
+            if (b){
+                return Result.success("保存成功");
+            }else
+                return Result.failure("调整失败，请重试。");
+        }else
+            return Result.failure("请确保计划项属于同一任务");
+    }
+//    删除计划项
+    @DeleteMapping("/{tp_id}/{pi_id}")
+    private Result deleteById(@PathVariable("pi_id") Long pi_id,@PathVariable("tp_id") Long tp_id){
+        Long authId = jwtUtils.getAuthId(request);
+        if (trainingPlanService.selectByUIdAndTPId(authId,tp_id)!=null){
+            service.removeById(pi_id);
+//            TODO 这里涉及到级联删除
+            return Result.success("删除成功");
+        }
+        else return Result.failure(ResultCode.AUTHERROR);
+    }
+//    查询计划项及其内容 TODO 结合后续的DayPlan 查询
+    @GetMapping("/{pi_id}")
+    private Result getById(@PathVariable Long pi_id){
+        return Result.success("接口待完成！");
+    }
+//    根据tp_id查找
+    @GetMapping("/")
+    private Result select(@RequestParam Long tp_id){
+        List<PlanItem> planItems = service.selectByTpId(tp_id);
+        return Result.success(planItems);
+    }
 }
 
--- src/main/java/com/zq/demo/service/impl/plan/PlanItemServiceImpl.java
+++ src/main/java/com/zq/demo/service/impl/plan/PlanItemServiceImpl.java
@@ -1,10 +1,15 @@
 package com.zq.demo.service.impl.plan;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zq.demo.pojo.plan.PlanItem;
 import com.zq.demo.dao.plan.PlanItemDao;
 import com.zq.demo.service.interfaces.plan.IPlanItemService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -16,5 +21,29 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PlanItemServiceImpl extends ServiceImpl<PlanItemDao, PlanItem> implements IPlanItemService {
+    @Resource
+    private PlanItemDao planItemDao;
+    //    创建计划项
+    public PlanItem init(PlanItem planItem){
+        planItemDao.insert(planItem);
+        return planItem;
+    }
 
+    //    根据pi_ids并通过tp_id分组判断是否有两组，有两组就说明非相同的计划项
+    public boolean isSameTp(Long[] pi_ids){
+        QueryWrapper<PlanItem> wrapper = new QueryWrapper<>();
+        wrapper.select("pi_tp_id","COUNT(*) as count")
+                .in("pi_id",pi_ids)
+                .groupBy("pi_tp_id");
+        List<Map<String, Object>> aaa = planItemDao.selectMaps(wrapper);
+
+        return aaa.size()==1;
+    }
+//    根据tp_id查找计划项
+    public List<PlanItem> selectByTpId(Long tp_Id){
+        QueryWrapper<PlanItem> wrapper = new QueryWrapper<>();
+        wrapper.eq("pi_tp_id",tp_Id)
+                .orderByAsc("pi_order");
+        return planItemDao.selectList(wrapper);
+    }
 }
